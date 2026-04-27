6d5b69911e31215737f1c10707e3d3b2be39bf0d
2deterburch <alinaplaydo@gmail.com>
2026-04-27 14:31:50 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+"use strict";
+
+export const user = {
+    name: "Аліна",
+    age: 18,
+    city: "Київ",
+    profession: "Студентка"
+};
+
+export const numbers = [10, 5, 8, 12];
+
+export const skills = ["HTML", "CSS", "JavaScript"];
